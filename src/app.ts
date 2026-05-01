82014cafc1101b2018df2fad151259e69975388d
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-05-01 11:43:57 +0200

    import and call setupMemberOnboarding before app starts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { App } from "@slack/bolt";
 import { startSchdeuledAnnouncements } from "./schedulers/reminders";
+import { setupMemberOnboarding } from "./listeners/member_joined";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN!,
@@ -8,6 +9,8 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN!,
   signingSecret: process.env.SLACK_SIGNING_SECRET!,
 });
+
+setupMemberOnboarding(app);
 
 app.message(async ({ message, say }) => {
   if ("text" in message && message.user) {
