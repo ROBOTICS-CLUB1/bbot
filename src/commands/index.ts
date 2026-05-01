9aca6b68065321e9878d66fb3b25580599756f90
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-05-01 12:32:00 +0200

    add registerPingCommand to central registration

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,8 @@
 import { App } from "@slack/bolt";
 import { registerHelpCommand } from "./help";
+import { registerPingCommand } from "./ping";
 
 export function registerCommands(app: App): void {
   registerHelpCommand(app);
+  registerPingCommand(app);
 }
