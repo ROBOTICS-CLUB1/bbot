e2751ce7dc435307d3da20d38c71e2d694cfa164
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-05-06 18:29:15 +0200

    feat: created manual type declarations for cron-parser   (parseExpression, CronExpression, CronDate, ParseOptions)

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
-import "dotenv/config";
-import { App } from "@slack/bolt";
-import { connectDB } from "./db";
-import { startDynamicScheduler } from "./schedulers/reminders";
-import { setupMemberOnboarding } from "./listeners/member_joined";
-import { registerCommands } from "./commands/index";
-
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN!,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN!,
-  signingSecret: process.env.SLACK_SIGNING_SECRET!,
-});
-
-setupMemberOnboarding(app);
-registerCommands(app);
-
-app.message(async ({ message, say }) => {
-  if ("text" in message && message.user) {
-    await say(`Hey <@${message.user}>, I got your message!`);
-  }
-});
-
-(async () => {
-  await connectDB();
-  await app.start();
-  console.log("[app] Bbot running in Socket Mode");
-  startDynamicScheduler(app);
+import "dotenv/config";
+import { App } from "@slack/bolt";
+import { connectDB } from "./db";
+import { startDynamicScheduler } from "./schedulers/reminders";
+import { setupMemberOnboarding } from "./listeners/member_joined";
+import { registerCommands } from "./commands/index";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN!,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN!,
+  signingSecret: process.env.SLACK_SIGNING_SECRET!,
+});
+
+setupMemberOnboarding(app);
+registerCommands(app);
+
+app.message(async ({ message, say }) => {
+  if ("text" in message && message.user) {
+    await say(`Hey <@${message.user}>, I got your message!`);
+  }
+});
+
+(async () => {
+  await connectDB();
+  await app.start();
+  console.log("[app] Bbot running in Socket Mode");
+  startDynamicScheduler(app);
 })();
